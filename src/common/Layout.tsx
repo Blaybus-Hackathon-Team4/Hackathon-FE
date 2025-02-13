f3691ff843025a6fe4df13fcc42bb9509d1a728c
hj7321 <oboki2001@naver.com>
2025-02-13 23:49:12 +0900

    [style] 레이아웃 CSS 지정정

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -1,15 +1,27 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
-    <>
-      <main>
+    <StWrapper>
+      <ContentWrapper>
         <Outlet />
-      </main>
+      </ContentWrapper>
       <Navbar />
-    </>
+    </StWrapper>
   );
 };
 
 export default Layout;
+
+const StWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* 전체 높이 확보 */
+`;
+
+const ContentWrapper = styled.main`
+  flex: 1; /* 남은 공간 차지 */
+  padding-bottom: 62.4px; /* Navbar 높이만큼 아래 여백 추가 */
+`;
