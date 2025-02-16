9fd6bce7447c2ca764af3c949f017b4b525b3d9c
hj7321 <oboki2001@naver.com>
2025-02-16 21:55:41 +0900

    [update] 테마 전역 공급을 위한 ThemeProvider 주입

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from "react-router";
+import { ThemeProvider } from "styled-components";
 import router from "./router/Router";
+import { theme } from "./styles/theme";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
