33971730df37210164ecc9867bc3de354ca3c835
xoxun <00xxs2@naver.com>
2025-02-16 18:44:08 +0900

    [style] GlobalStyle basic font color

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router";
 import router from "./router/Router";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    color: ${({ theme }) => theme.colors.gray[900]}; // 기본 글씨 색상
+  }
+`;
+
+export default GlobalStyle;
