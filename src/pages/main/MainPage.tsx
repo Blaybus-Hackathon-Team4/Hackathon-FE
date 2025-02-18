23d854a042111763ead8456e13e7231bcc1535c0
hj7321 <oboki2001@naver.com>
2025-02-18 23:10:18 +0900

    단순 코드 위치 변경

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import HeroSection from "./components/HeroSection";
 import CategoryTabs from "./components/CategoryTabs";
+import HeroSection from "./components/HeroSection";
 import TopDesigners from "./components/TopDesigners";
 import TrendingStyles from "./components/TrendingStyles";
 
