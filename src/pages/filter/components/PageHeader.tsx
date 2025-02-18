8adef96d8b299ad1a41828809b363208a6779399
huioid <huijaelee2020@gmail.com>
2025-02-19 02:41:53 +0900

    [fix] 닫기 버튼 깨짐 수정

--- src/pages/filter/components/PageHeader.tsx
+++ src/pages/filter/components/PageHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import exit from "../../assets/icons/exit.svg";
+import exit from "../../../assets/icons/exit.svg";
 
 interface PageHeaderProps {
   title?: string;
