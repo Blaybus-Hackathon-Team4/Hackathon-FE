d159d639438c199b3c1fd460731402e1c88ed026
hj7321 <oboki2001@naver.com>
2025-02-18 23:16:47 +0900

    [bugfix] 타입 미지정 오류로 인한 주석 처리

--- src/api/schedule.api.ts
+++ src/api/schedule.api.ts
@@ -29,25 +29,25 @@ export const GetDesignerSchedule = async () => {
   }
 };
 
-export const UpdateDesignerSchedule = async (reservationData) => {
-  const path = "/api/v1/schedule/updateDesignerTimeSchedule";
+// export const UpdateDesignerSchedule = async (reservationData) => {
+//   const path = "/api/v1/schedule/updateDesignerTimeSchedule";
 
-  try {
-    const response = await api.patch(path, reservationData);
-    const data = response.data;
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.error(
-        "[UpdateDesignerSchedule] 디자이너 스케줄 수정하기 실패: ",
-        error.response?.data || "응답 없음"
-      );
-    } else {
-      console.error(
-        "[UpdateDesignerSchedule] 디자이너 스케줄 수정하기 실패 - 알 수 없는 에러: ",
-        error
-      );
-    }
-    throw error;
-  }
-};
+//   try {
+//     const response = await api.patch(path, reservationData);
+//     const data = response.data;
+//     return data;
+//   } catch (error) {
+//     if (error instanceof AxiosError) {
+//       console.error(
+//         "[UpdateDesignerSchedule] 디자이너 스케줄 수정하기 실패: ",
+//         error.response?.data || "응답 없음"
+//       );
+//     } else {
+//       console.error(
+//         "[UpdateDesignerSchedule] 디자이너 스케줄 수정하기 실패 - 알 수 없는 에러: ",
+//         error
+//       );
+//     }
+//     throw error;
+//   }
+// };
