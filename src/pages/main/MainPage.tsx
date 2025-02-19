5fb993900d4a51c982d7ce1577d3ae1809b0020b
xoxun <00xxs2@naver.com>
2025-02-19 13:03:34 +0900

    [fix] 테스트 복귀

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -30,7 +30,6 @@ const MainPage: React.FC = () => {
   getDesignerList();
   return (
     <MainContainer>
-      <img src="/1004.jpg" />
       <Header />
       <HeroSection />
       <CategoryTabs />
