3aa40189a8bfe7590c003b5a1453d25e7ed0643f
hj7321 <oboki2001@naver.com>
2025-02-19 18:55:04 +0900

    [delete] 로그인 관련 API 로직 필요 없는 부분 삭제제

--- src/api/login.api.ts
+++ src/api/login.api.ts
@@ -1,36 +1,15 @@
 import { AxiosError } from "axios";
 import { api } from "./api";
 
-export const GetUserData = async (accessToken: string) => {
+export const GetJwtToken = async (email: string, name: string) => {
   const path = "/auth/google";
 
   try {
-    const response = await api.post(path, { authCode: accessToken });
+    const response = await api.post(path, {
+      email,
+      name,
+    });
     const data = response.data;
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.error(
-        "[GetUserData] 사용자 정보 가져오기 실패: ",
-        error.response?.data || "응답 없음"
-      );
-    } else {
-      console.error(
-        "[GetUserData] 사용자 정보 가져오기 실패 - 알 수 없는 에러: ",
-        error
-      );
-    }
-    throw error;
-  }
-};
-
-export const GetJwtToken = async () => {
-  const path = "/auth/login-success";
-
-  try {
-    const response = await api.get(path);
-    const data = response.data;
-    console.log(data);
     return data;
   } catch (error) {
     if (error instanceof AxiosError) {
