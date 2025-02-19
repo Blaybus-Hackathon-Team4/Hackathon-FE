8bce84fc21e961dcac74c71e664492862d1758dd
hj7321 <oboki2001@naver.com>
2025-02-19 18:57:38 +0900

    [bugfix, update] 구글 소셜 로그인 구현

--- src/pages/login/components/LoginButton.tsx
+++ src/pages/login/components/LoginButton.tsx
@@ -1,53 +1,86 @@
-import { useGoogleLogin } from "@react-oauth/google";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { GetUserData } from "../../../api/login.api";
-import UserIcon from "../../../assets/icons/user.svg";
+import { GetJwtToken } from "../../../api/login.api";
+import "../../../styles/google-login.css";
 import { useUserStore } from "../../../zustand/user.store";
+
+interface GoogleJwtPayload extends JwtPayload {
+  name?: string;
+  email?: string;
+}
 
 const LoginButton = () => {
   const navigate = useNavigate();
   const { setLogin } = useUserStore();
 
-  // Google 로그인 성공 시 실행되는 함수
-  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      console.log("구글 로그인 성공", response);
-      // 액세스 토큰 로컬 스토리지에 저장
-      localStorage.setItem("accessToken", response.access_token);
-      // 액세스 토큰을 서버에 보내서 사용자 정보를 가져옴 -> 아이디 토큰이랑 액세스 토큰 다른가?
-      const userData = await GetUserData(response.access_token);
-      setLogin(userData.email, userData.name);
-      navigate("/"); // 예시: 홈 페이지로 이동
-    },
-    onError: () => {
-      console.error("구글 로그인 실패");
-    },
-  });
+  const successLogin = async (credentialResponse: CredentialResponse) => {
+    const decodingData = jwtDecode<GoogleJwtPayload>(
+      credentialResponse.credential as string
+    );
+    const { name, email } = decodingData;
+    try {
+      const data = await GetJwtToken(email as string, name as string);
+      setLogin(name as string, email as string);
+      sessionStorage.setItem("accessToken", data.jwt); // JWT 토큰 저장
+      navigate("/"); // 로그인 후 홈으로 이동
+    } catch (error) {
+      console.error("JWT 토큰 가져오기 실패", error);
+    }
+  };
+
+  const failedLogin = () => {
+    console.log("Login Failed");
+  };
 
   return (
-    <div>
-      <Button onClick={() => login()}>
+    <StDiv>
+      {/* <Button id="login-icon">
         <img src={UserIcon} alt="login" width={24.96} height={24.96} />
         <StParagraph>로그인</StParagraph>
-      </Button>
-    </div>
+      </Button> */}
+      <GoogleLogin
+        onSuccess={successLogin}
+        onError={failedLogin}
+        width="0px"
+        logo_alignment="center"
+        size="large"
+        // type="icon"
+        containerProps={{
+          id: "google-login-button", // id 속성 추가
+          className: "my-custom-class", // 클래스 추가
+          style: {
+            width: "45px",
+            height: "40px",
+          }, // 스타일 추가
+        }}
+      />
+    </StDiv>
   );
 };
 
 export default LoginButton;
 
-const Button = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 45.76px;
-  height: 45.76px;
-  gap: 1px;
-  cursor: pointer;
+const StDiv = styled.div`
+  position: relative;
 `;
 
-const StParagraph = styled.p`
-  font-size: 12.48px;
-  color: ${({ theme }) => theme.colors.gray[300]};
-`;
+// const Button = styled.div`
+//   position: absolute;
+//   background-color: white;
+//   right: 0;
+//   /* z-index: 500; */
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   width: 45.76px;
+//   height: 45.76px;
+//   gap: 1px;
+//   cursor: pointer;
+// `;
+
+// const StParagraph = styled.p`
+//   font-size: 12.48px;
+//   color: ${({ theme }) => theme.colors.gray[300]};
+// `;
