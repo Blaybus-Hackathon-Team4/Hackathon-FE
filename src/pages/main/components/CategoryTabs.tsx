a50eeb7699087be90a96677f6ffb1cbf8a3acc71
Roflaff <kimsp0317@gmail.com>
2025-02-19 19:39:01 +0900

    field 추가

--- src/pages/main/components/CategoryTabs.tsx
+++ src/pages/main/components/CategoryTabs.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useNavigate } from "react-router-dom";
+
 import cutIcon from "../../../assets/icons/main-cut-icon.svg";
 import dyeingIcon from "../../../assets/icons/main-dyeing-icon.svg";
 import magicIcon from "../../../assets/icons/main-magic-icon.svg";
 import permIcon from "../../../assets/icons/main-perm-icon.svg";
 
 const categories = [
-  { name: "커트", imgLink: cutIcon },
-  { name: "펌", imgLink: permIcon },
-  { name: "매직", imgLink: magicIcon },
-  { name: "탈염색", imgLink: dyeingIcon },
+  { name: "커트", imgLink: cutIcon, field: "커트" },
+  { name: "펌", imgLink: permIcon, field: "펌" },
+  { name: "매직", imgLink: magicIcon, field: "매직" },
+  { name: "탈염색", imgLink: dyeingIcon, field: "탈염색" },
 ];
 
+
 const CategoryTabs: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (field: string) => {
+    navigate("/designer-list", { state: { field } });
+  };
+
   return (
     <TabsContainer>
       {categories.map((category) => (
-        <Tab key={category.name}>
-          <TabImg src={category.imgLink} alt={category.name}></TabImg>
+        <Tab key={category.name} onClick={() => handleCategoryClick(category.field)}>
+          <TabImg src={category.imgLink} alt={category.name} />
           <TabText>{category.name}</TabText>
         </Tab>
       ))}
