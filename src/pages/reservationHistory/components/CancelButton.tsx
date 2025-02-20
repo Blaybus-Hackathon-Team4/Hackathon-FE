3e3b690b181ea4ccc2e3e75818614a6c11798c20
huioid <huijaelee2020@gmail.com>
2025-02-20 13:02:56 +0900

    [Update] 예약 취소 버튼

--- src/pages/reservationHistory/components/CancelButton.tsx
+++ src/pages/reservationHistory/components/CancelButton.tsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { useParams } from "react-router";
 import styled from "styled-components";
 import { theme } from "../../../styles/theme";
 import { useModalStore } from "../../../zustand/modal.store";
-import CancelModal from "./CancelModal";
 
 interface CancelButtonProps {
   onClick?: () => void;
 }
 
 const CancelButton: React.FC<CancelButtonProps> = ({ onClick }) => {
-  const { openModal } = useModalStore();
-  const { paymentId } = useParams<{ paymentId: string }>();
+  const { openCancelModal } = useModalStore();
 
-  // 여기서 Modal 컴포넌트를 정의하여 openModal에 전달
-  const handleOpenModal = () => {
-    const modalElement = <CancelModal paymentId={paymentId!} />;
-    openModal(modalElement);
-  };
-
-  return <Button onClick={onClick || handleOpenModal}>예약 취소</Button>;
+  return <Button onClick={onClick || openCancelModal}>예약 취소</Button>;
 };
 
 export default CancelButton;
